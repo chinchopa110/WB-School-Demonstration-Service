bd9c861540c81a3d49ca22c08c7d6ab96018f15d
polyakov <s_zhopa88@mail.ru>
2025-03-15 01:04:31 +0300

    fix: singleton sql connection

--- internal/configs/sqlConfig.go
+++ internal/configs/sqlConfig.go
@@ -5,9 +5,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func GetUpSQL() (*sql.DB, error) {
+	if db != nil {
+		return db, nil
+	}
+	
 	connStr := "user=postgres password=123 dbname=wb1 sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
